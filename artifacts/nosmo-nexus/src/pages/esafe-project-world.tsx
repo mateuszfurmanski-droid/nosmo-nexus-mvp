b7af484c17f91671287aecfb064b8f29765cd3e6
mateuszfurmanski-droid <mateusz.furmanski@gmail.com>
2026-08-13 05:34:40 +0100

    Remove duplicate PersistentWorkspace top chrome in e-SAFE shell

--- artifacts/nosmo-nexus/src/pages/esafe-project-world.tsx
+++ artifacts/nosmo-nexus/src/pages/esafe-project-world.tsx
@@ -39,7 +39,9 @@ export default function EsafeProjectWorld() {
       <NexusGraphCommandBridge />
       <style>{`
         [data-control][class*="bottom-3 left-3 right-3 z-50"],
-        [data-control][class*="bottom-3 left-3 z-40"] {
+        [data-control][class*="bottom-3 left-3 z-40"],
+        [data-control][class*="absolute left-3 top-3 z-50"],
+        [data-control][class*="absolute left-1/2 top-3 z-40"] {
           display: none !important;
         }
 
