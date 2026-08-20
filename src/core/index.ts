81b5f5dc5544fae947690ff6c47dfd3a4aafa291
mateuszfurmanski-droid <mateusz.furmanski@gmail.com>
2026-08-20 21:16:33 +0100

    Add Nexus MVP core app skeleton

---
+++ src/core/index.ts
@@ -0,0 +1,23 @@
+export * from './coreContract';
+export * from './shell/shellContract';
+export * from './graph/graphContract';
+export * from './timeline/timelineContract';
+export * from './events/eventBus';
+export * from './permissions/permissionContract';
+export * from './storage/storageContract';
+
+import { nexusShellCore } from './shell/shellContract';
+import { nexusGraphCore } from './graph/graphContract';
+import { nexusTimelineCore } from './timeline/timelineContract';
+import { nexusEventsCore } from './events/eventBus';
+import { nexusPermissionsCore } from './permissions/permissionContract';
+import { nexusStorageCore } from './storage/storageContract';
+
+export const nexusCoreContracts = [
+  nexusShellCore,
+  nexusGraphCore,
+  nexusTimelineCore,
+  nexusEventsCore,
+  nexusPermissionsCore,
+  nexusStorageCore,
+];
