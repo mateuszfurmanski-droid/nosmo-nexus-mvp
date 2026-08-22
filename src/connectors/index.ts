90afaad5693435f5734231802f242be69683e1f5
mateuszfurmanski-droid <mateusz.furmanski@gmail.com>
2026-08-22 15:18:38 +0100

    Export bounded SpatialConnector hand-off contract

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,6 +1,23 @@
 export { googleDriveConnector } from './google-drive/googleDriveConnector';
 export { workWalletConnector } from './work-wallet/workWalletConnector';
 export { bimFabstationConnector } from './bim-fabstation/bimFabstationConnector';
+export {
+  FABSTATION_CANDIDATE_DESCRIPTOR,
+  MAX_SPATIAL_HAND_OFF_JSON_BYTES,
+  NEXUS_SPATIAL_HAND_OFF_SCHEMA,
+  createNexusSpatialHandOff,
+} from './bim-fabstation/spatialHandoff';
+export type {
+  NexusSpatialConnectorClaimStatus,
+  NexusSpatialConnectorMaturity,
+  NexusSpatialHandOffInput,
+  NexusSpatialHandOffPacket,
+  NexusSpatialHandOffResolution,
+  NexusSpatialHandoffSource,
+  NexusSpatialOperationalContext,
+  NexusSpatialOperationalState,
+  NexusSpatialPartnerDescriptor,
+} from './bim-fabstation/spatialHandoff';
 export { companyCamConnector } from './companycam/companyCamConnector';
 export { hiltiConnector } from './hilti/hiltiConnector';
 export { microsoft365Connector } from './microsoft365/microsoft365Connector';
