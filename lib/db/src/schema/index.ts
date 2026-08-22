0a4bb9ee027c9b35a049b6a9ae8daeb4ba4bc398
mateuszfurmanski-droid <mateusz.furmanski@gmail.com>
2026-08-22 16:01:26 +0100

    Export shared Project Memory identity schema

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -9,5 +9,5 @@ export * from "./notes";
 export * from "./conversations";
 export * from "./files";
 export * from "./nexusProjectMemoryCloud";
-export * from "./nexusRuntimeIdentity";
+export * from "./nexusProjectMemoryIdentity";
 export * from "./nexusProjectAccess";
