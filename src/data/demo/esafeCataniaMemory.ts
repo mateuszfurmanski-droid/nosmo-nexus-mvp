64f00e5255a844873ab7a2cca5906e7cc3ccc1ee
mateuszfurmanski-droid <mateusz.furmanski@gmail.com>
2026-08-24 18:31:49 +0100

    Compose e-SAFE core WorkSuite fixtures

--- src/data/demo/esafeCataniaMemory.ts
+++ src/data/demo/esafeCataniaMemory.ts
@@ -1,12 +1,15 @@
 import type { NexusProjectMemorySnapshot } from '../projectMemory';
 import { emptyProjectMemorySnapshot } from '../projectMemory';
 import { assertEsafeCataniaMemory } from '../projectMemoryInvariants';
+import { applyEsafeCataniaCoreWorkFixtures } from './esafeCataniaCoreWorkFixtures';
 import { applyEsafeCataniaFixtures } from './esafeCataniaFixtures';
 import { applyEsafeCataniaPhase9Fixtures } from './esafeCataniaPhase9Fixtures';
 
 export const createEsafeCataniaMemory = (): NexusProjectMemorySnapshot => {
-  const memory = applyEsafeCataniaPhase9Fixtures(
-    applyEsafeCataniaFixtures(emptyProjectMemorySnapshot()),
+  const memory = applyEsafeCataniaCoreWorkFixtures(
+    applyEsafeCataniaPhase9Fixtures(
+      applyEsafeCataniaFixtures(emptyProjectMemorySnapshot()),
+    ),
   );
 
   assertEsafeCataniaMemory(memory);
